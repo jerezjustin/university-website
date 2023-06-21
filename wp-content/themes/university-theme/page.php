5688055db714a1350239c99e311c919b301c1ca4
Justin Jerez <justinjerez@hotmail.com>
2023-06-21 10:58:21 -0400

    Dynamic breadcrum and child pages navigation

--- wp-content/themes/university-theme/page.php
+++ wp-content/themes/university-theme/page.php
@@ -1,12 +1,12 @@
 <?php
 get_header();
 
-while(have_posts()) {
+while (have_posts()) {
     the_post()
 ?>
     <div class="page-banner">
         <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-        
+
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?= the_title() ?></h1>
             <div class="page-banner__intro">
@@ -17,26 +17,52 @@ while(have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
+        <?php
+        $parentId = wp_get_post_parent_id(get_the_ID());
 
-        <!-- <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        if ($parentId) {
+        ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?= get_permalink($parentId) ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                        Back to <?= get_the_title($parentId) ?>
+                    </a>
 
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-        </div> -->
+                    <span class="metabox__main"><?= the_title() ?></span>
+                </p>
+            </div>
+        <?php
+        }
+
+        if ($parentId or get_pages(['child_of' => get_the_ID()])) {
+        ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?= get_permalink($parentId) ?>"><?= get_the_title($parentId) ?></a></h2>
+
+                <ul class="min-list">
+                    <?php
+                    if ($parentId) {
+                        $childrenOf = $parentId;
+                    } else {
+                        $childrenOf = get_the_ID();
+                    }
+
+                    wp_list_pages([
+                        'title_li' => NULL,
+                        'child_of' => $childrenOf,
+                        'sort_column' => 'menu_order'
+                    ]);
+                    ?>
+                </ul>
+            </div>
+        <?php } ?>
 
         <div class="generic-content">
             <?= the_content() ?>
         </div>
     </div>
-<?php   
+<?php
 }
 
 get_footer();
